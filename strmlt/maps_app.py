ce62c9c5a157ffd1104e8ca163f73f6b23d0c272
Joonas Kitunen <joonaskitunen@kamk.fi>
2025-12-09 20:27:31 +0200

    Changes to streamlit file sturcture

--- strmlt/maps_app.py
+++ strmlt/maps_app.py
@@ -24,8 +24,8 @@ pdk.settings.mapbox_api_key = MAPBOX_TOKEN
 API_URL = os.getenv("API_URL", "http://localhost:8000")
 
 # 2. Tuodaan funktiot
-from here_client import geocode, route, parse_traffic_incidents
-from digitraffic_client import (
+from utils.here_client import geocode, route, parse_traffic_incidents
+from utils.digitraffic_client import (
     get_weather_cameras, 
     traffic_messages_near_route, 
     get_road_weather_stations, 
@@ -34,7 +34,7 @@ from digitraffic_client import (
     get_lam_stations,
     get_road_weather_history
 )
-from weather_client import get_rainviewer_data, get_closest_timestamp
+from utils.weather_client import get_rainviewer_data, get_closest_timestamp
 
 # ====================================================================
 # CALENDAR HELPERS
@@ -47,8 +47,10 @@ def _add_ical_events(url: str):
     try:
         # Tässä oletetaan, että meillä on joku API endpoint joka palauttaa JSONia iCal URLista
         # Mutta koska api_server.py:ssä on /ical/events, käytetään sitä jos mahdollista.
-        # Oletetaan, että api_server on pystyssä localhost:8000.
-        resp = requests.get("http://localhost:8000/ical/events", params={"url": url}, timeout=5)
+        # Oletetaan, että api_server on pystyssä localhost:8000. 
+
+        # Korjattu localhost -> api, ovat samassa verkossa dockerissa, localhost osoittaa dockerin itseensä, api on toisessa dockerissa
+        resp = requests.get("http://api:8000/ical/events", params={"url": url}, timeout=5)
         if resp.status_code == 200:
             data = resp.json()
             for event in data:
@@ -834,7 +836,7 @@ with st.sidebar:
             ).properties(height=200).add_params(selection)
 
             # Render single chart (no layers)
-            chart_selection = st.altair_chart(base, width="stretch", theme="streamlit", on_select="rerun")
+            chart_selection = st.altair_chart(base, theme="streamlit", on_select="rerun")
             
             # Handle selection to update slider
             if len(chart_selection["selection"]) > 0:
